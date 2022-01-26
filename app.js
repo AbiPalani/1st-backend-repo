d11df1a398732b0bc468d7f0c01a67467420b19f
AbiPalani <abipalani0610@gmail.com>
2022-01-26 15:30:47 +0530

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require("cors");
 const userRoutes = require("./routes/user.route");
 const eventRoutes = require("./routes/event.route");
-const { authentication ,ignoreFavicon} = require("./services/verification.services");
+const {ignoreFavicon} = require("./services/verification.services");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3001;
     app.use(cors());
     app.use(express.json());
     app.use("/users", userRoutes);
-    app.use(authentication);
+    //app.use(authentication);
     app.use("/events", eventRoutes);
     app.listen(PORT, () => {
       console.log("Server running in PORT", PORT);
