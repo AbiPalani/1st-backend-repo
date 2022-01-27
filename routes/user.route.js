916a63e38fc950b2f4e4c7223c91405a4ab39021
AbiPalani <abipalani0610@gmail.com>
2022-01-27 13:50:55 +0530

    make it better

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,6 @@
 const user_route = require("express").Router();
 const userService= require("../services/user.services");
 
-user_route.get('/',function (req, res, next) {
-    res.status(200).send("Welcome users");
-    next();
-  })
 user_route.post("/register",userService.register);
 user_route.post("/login",userService.login);
 
