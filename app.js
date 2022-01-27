51632b3f26cf8ba4c991966915d46354a58c0ee9
AbiPalani <abipalani0610@gmail.com>
2022-01-27 13:32:00 +0530

    make it better

--- app.js
+++ app.js
@@ -15,12 +15,15 @@ const app=express();
     try{
         await mongo.connect();
         app.use(ignoreFavicon);
-        const corsOptions =  function (req, res) {
-          res.setHeader("Access-Control-Allow-Origin", "*");
-          res.setHeader('Access-Control-Allow-Methods', '*');
-          res.setHeader("Access-Control-Allow-Headers", "*");
-          res.end();
-        };
+        app.use(function(req, res, next) {
+          res.header("Access-Control-Allow-Origin", '*');
+          res.header("Access-Control-Allow-Credentials", true);
+          res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+          res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+          next();
+        });
+
+        app.use(cors()); // <---- use cors middleware
         app.use(express.json());     
         app.use("/users", cors(corsOptions),userRoutes);
         app.use(authentication);
