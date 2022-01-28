a5ccef9aab32799399c0c465fbb6801ffb6a2050
AbiPalani <abipalani0610@gmail.com>
2022-01-28 14:16:09 +0530

    make it better

--- shared/mongo.js
+++ shared/mongo.js
@@ -1,22 +1,24 @@
 const MongoClient = require("mongodb").MongoClient;
-const URI = "mongodb+srv://diary-app:diary@123@cluster0.tyua0.mongodb.net/diary?retryWrites=true&w=majority";
+
+const URI = "mongodb+srv://diary-app:diary123@cluster0.tyua0.mongodb.net/diary?retryWrites=true&w=majority";
 const client = new MongoClient(URI);
 
 module.exports ={
   //complete conection
-  db:null,
-  //connection to particular collections
-  events:null,
-  users:null,
-   async connect(){
-    //connection to database
-   await client.connect();
-   this.db=client.db(process.env.DB_Name);
-   this.events = this.db.collection("events");
-   this.users = this.db.collection("users");
-}
-}
+    db:null,
 
+    //connection to particular collections
+    events:null,
+    users:null,
+
+     async connect(){
+      //connection to database
+     await client.connect();
+     this.db=client.db(process.env.DB_Name);
+     this.events = this.db.collection("events");
+     this.users = this.db.collection("users");
+  },
+};
 
 
 
