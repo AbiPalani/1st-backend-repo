7890ae5cb074dd6198f62132a1b9b842001437fd
AbiPalani <abipalani0610@gmail.com>
2022-01-29 16:27:56 +0530

    make it better

--- routes/event.route.js
+++ routes/event.route.js
@@ -3,8 +3,8 @@ const eventService= require("../services/event.services");
 
 event_route.get("/view",eventService.findEvent);
 event_route.post("/add",eventService.insertEvent);
-event_route.put("/add/:_id",eventService.updateEvent);
-event_route.delete("/view/:_id",eventService.deleteEvent);
+event_route.put("/add",eventService.updateEvent);
+event_route.delete("/view",eventService.deleteEvent);
 
 
 module.exports = event_route;
