73c31c42fbe925a926aafe9e5afdc16b8c83ee4e
AbiPalani <abipalani0610@gmail.com>
2022-01-29 12:48:58 +0530

    make it better

--- routes/event.route.js
+++ routes/event.route.js
@@ -1,8 +1,8 @@
 const event_route = require("express").Router();
 const eventService= require("../services/event.services");
 
-event_route.get("/",eventService.findEvent);
-event_route.post("/",eventService.insertEvent);
+event_route.get("/view",eventService.findEvent);
+event_route.post("/add",eventService.insertEvent);
 event_route.put("/:_id",eventService.updateEvent);
 event_route.delete("/:_id",eventService.deleteEvent);
 
